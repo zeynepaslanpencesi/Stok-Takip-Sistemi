b82aed725cd9df5369054f36dd002379808dfb82
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:36:21 +0300

    FirmaController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Firma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Firma
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public int AddresId { get; set; }
+    }
+}
