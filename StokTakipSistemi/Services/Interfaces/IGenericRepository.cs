767d058a5bebf91931378d932f3d1948d9170854
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 11:38:29 +0300

    Services klasörü oluşturuldu Services klasörü içersinde arayüzlerin bulunacağı Interfaces klasörü oluşturuldu Interfaces içerisinde IGenericRepository interface'i oluşturuldu

---
+++ StokTakipSistemi/Services/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services.Interfaces
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> predicate);
+        Task<T> Get(int? id);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(T item);
+        Task Delete(int? id);
+        Task<bool> IsExist(Expression<Func<T, bool>> expression);
+    }
+}
