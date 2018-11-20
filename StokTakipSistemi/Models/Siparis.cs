5f7530e2b8907cac925abc8974c8ab06fbf52d4d
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:47:15 +0300

    SiparisController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Siparis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Siparis
+    {
+        public int Id { get; set; }
+        public int Adet { get; set; }
+        public int FaturaId { get; set; }
+        public int UrunId { get; set; }
+    }
+}
