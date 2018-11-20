63f445505226c5f5ce29235591c279e344fc7dc4
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 17:08:29 +0300

    StokTakipSistemiDbContext sınıfı içerisinde veritabanının tabloları oluşturuldu

--- StokTakipSistemi/Data/StokTakipSistemiDbContext.cs
+++ StokTakipSistemi/Data/StokTakipSistemiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StokTakipSistemi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,21 @@ namespace StokTakipSistemi.Data
         public StokTakipSistemiDbContext(DbContextOptions<StokTakipSistemiDbContext> options) : base(options)
         {
         }
-
+        public DbSet<Urun> Urun { get; set; }
+        public DbSet<UrunTur> UrunTur { get; set; }
+        public DbSet<Marka> Marka { get; set; }
+        public DbSet<Sehir> Sehir { get; set; }
+        public DbSet<Ilce> Ilce { get; set; }
+        public DbSet<Adres> Adres { get; set; }
+        public DbSet<Firma> Firma { get; set; }
+        public DbSet<Kullanici> Kullanici { get; set; }
+        public DbSet<Fatura> Fatura { get; set; }
+        public DbSet<Siparis> Siparis { get; set; }         
+        public DbSet<UrunSaglayici> UrunSaglayici { get; set; }
+        public DbSet<Departman> Departman { get; set; }
+        public DbSet<Unvan> Unvan { get; set; }
+       
+        
 
 
 
