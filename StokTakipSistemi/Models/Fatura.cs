a3051536c3ddcf04591c31c9c29152d81144ab53
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:46:14 +0300

    FaturaController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Fatura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Fatura
+    {
+        public int Id { get; set; }
+        public DateTime Tarih { get; set; }
+        public int UrunSaglayiciId { get; set; }
+    }
+}
