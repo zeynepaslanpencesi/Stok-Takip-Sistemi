1c6e38dbfba0642698b925651e4f2663e9c9467c
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:48:10 +0300

    DepartmnController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Departman.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Departman
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Adi { get; set; }
+    }
+}
