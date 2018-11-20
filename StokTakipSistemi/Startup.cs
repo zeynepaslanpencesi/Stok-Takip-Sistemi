8479387361783707a0701ef22e7b6c09dd49b38b
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 11:34:06 +0300

    Proje ile gelen HomeController sınıfı silindi Home View, Adres View olarak değiştirildi Startup.cs içerisinde uygulamaya çalıştırıldığında yönlendirilecek sayfa ayarlandı

--- StokTakipSistemi/Startup.cs
+++ StokTakipSistemi/Startup.cs
@@ -43,7 +43,7 @@ namespace StokTakipSistemi
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Adres}/{action=Index}/{id?}");
             });
         }
     }
