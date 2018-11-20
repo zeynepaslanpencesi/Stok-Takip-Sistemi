e806f6f823c00f200539191bd19caf8f6527f813
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:50:15 +0300

    UrunTurController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/UrunTur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class UrunTur
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+    }
+}
