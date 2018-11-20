d4d77374bfe488be76f74159702d79fd1c4cca4b
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:49:26 +0300

    UrunController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public decimal Fiyat { get; set; }
+        public int UrunTurId { get; set; }
+        public int MarkaId { get; set; }
+        public int Adet { get; set; }
+    }
+}
