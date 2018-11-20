dbfc4aa577c18e42be783a78cdd0e6192849b154
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-20 16:39:59 +0300

    UnvanController için Model oluşturuldu

---
+++ StokTakipSistemi/Models/Unvan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Models
+{
+    public class Unvan
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public byte Indirim { get; set; }
+    }
+}
