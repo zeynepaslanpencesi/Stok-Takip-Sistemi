1c14d6184e32f5fbdc547a9a4958549c46fc7b01
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-24 16:03:57 +0300

    ISehirService interface'i oluşturuldu

--- StokTakipSistemi/Controllers/SehirController.cs
+++ StokTakipSistemi/Controllers/SehirController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
+    [Authorize]
     public class SehirController : Controller
     {
+        private readonly ISehirService _service;
+
+        public SehirController(ISehirService sehirService)
+        {
+            _service = sehirService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _service.GetAll();
+            return View(items);
+           
         }
     }
 }
---
+++ StokTakipSistemi/Services/Interfaces/ISehirService.cs
@@ -0,0 +1,13 @@
+﻿using StokTakipSistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services.Interfaces
+{
+
+    public interface ISehirService : IGenericRepository<Sehir>
+    {
+    }
+}
