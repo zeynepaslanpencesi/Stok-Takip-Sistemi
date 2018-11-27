5c5eee95780258b8b82b685bf1e06125cf658976
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 11:25:30 +0300

    IlceController sınıfının Create action'ı için HttpGet ve HttpPost metotları oluşturukdu

--- StokTakipSistemi/Controllers/IlceController.cs
+++ StokTakipSistemi/Controllers/IlceController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Models;
 using StokTakipSistemi.Services.Interfaces;
+using StokTakipSistemi.ViewModels;
 
 namespace StokTakipSistemi.Controllers
 {
@@ -21,5 +24,31 @@ namespace StokTakipSistemi.Controllers
             return View(items);
             
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] IlceVM ilce)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSameSehirExists = await _ilceService.IsExist(c => c.Adi == ilce.Adi);
+
+                if (!isSameSehirExists)
+                {
+                    var mappedIlce = Mapper.Map<Ilce>(ilce);
+                    await _ilceService.Create(mappedIlce);
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
+
+        }
+
     }
 }
