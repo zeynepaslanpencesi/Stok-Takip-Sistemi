2554bccddcdb87673b18216c68e532d2992e6782
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 12:31:52 +0300

    DepartmanController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/DepartmanController.cs
+++ StokTakipSistemi/Controllers/DepartmanController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class DepartmanController : Controller
     {
+        private readonly IDepartmanService _departmanService;
+        public DepartmanController(IDepartmanService departmanService)
+        {
+            _departmanService = departmanService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _departmanService.GetAll();
+            return View(items);
         }
     }
 }
