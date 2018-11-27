5a90aa509fec0c6ce8624decefc9053f62669550
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 11:56:41 +0300

    AdresController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/AdresController.cs
+++ StokTakipSistemi/Controllers/AdresController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class AdresController : Controller
     {
+        private readonly IAdresService _adresService;
+
+        public AdresController(IAdresService adresService)
+        {
+            _adresService = adresService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _adresService.GetAll();
+            return View(items);
+            
         }
     }
 }
