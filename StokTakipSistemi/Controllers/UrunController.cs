983ec7db06a508114aa623fbae2b12f5f01bc5af
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 13:32:58 +0300

    UrunController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/UrunController.cs
+++ StokTakipSistemi/Controllers/UrunController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class UrunController : Controller
     {
+        private readonly IUrunService _urunService;
+        
+
+        public UrunController(IUrunService urunService)
+        {
+            _urunService = urunService;
+           
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _urunService.GetAll();
+            return View(items);
+
         }
     }
 }
