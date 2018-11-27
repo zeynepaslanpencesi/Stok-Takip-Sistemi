ca3cb512c078d5f5dce32a732c3c6d246a13880d
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 12:04:23 +0300

    AdresController sınıfının Create action'ı için HttpGet ve HttpPost metotları oluşturuldu

--- StokTakipSistemi/Controllers/AdresController.cs
+++ StokTakipSistemi/Controllers/AdresController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Models;
 using StokTakipSistemi.Services.Interfaces;
+using StokTakipSistemi.ViewModels;
 
 namespace StokTakipSistemi.Controllers
 {
@@ -20,6 +23,48 @@ namespace StokTakipSistemi.Controllers
             var items = _adresService.GetAll();
             return View(items);
             
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] AdresVM adres)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSameAddressExists = await _adresService.IsExist(c => c.AdresText == adres.AdresText);
+
+                if (!isSameAddressExists)
+                {
+                    var mappedAdres = Mapper.Map<Adres>(adres);
+                    await _adresService.Create(mappedAdres);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemToUpdate = await _adresService.Get(id);
+
+            if (itemToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemToUpdate);
         }
     }
 }
