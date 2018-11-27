8cd13bd63b60ed8c672dda530ce9ba99ff5ea784
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 12:21:47 +0300

    AdresController sınıfının Delete action'ı oluşturuldu

--- StokTakipSistemi/Controllers/AdresController.cs
+++ StokTakipSistemi/Controllers/AdresController.cs
@@ -84,5 +84,23 @@ namespace StokTakipSistemi.Controllers
 
             return NotFound();
         }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemToDelete = await _adresService.Get(id);
+
+            if (itemToDelete != null)
+            {
+                await _adresService.Delete(id);
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
     }
 }
