d13af4fef9439fb6c5ea202e5dc3bc34ee7cc47d
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 13:26:55 +0300

    UrunVM oluşturuldu

---
+++ StokTakipSistemi/ViewModels/UrunVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.ViewModels
+{
+    public class UrunVM
+    {
+        public string Adi { get; set; }
+        public decimal Fiyat { get; set; }
+        public int UrunTurId { get; set; }
+        public int MarkaId { get; set; }
+        public int Adet { get; set; }
+    }
+}
