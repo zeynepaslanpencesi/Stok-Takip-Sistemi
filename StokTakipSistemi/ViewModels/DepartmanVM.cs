8fe2bf58dd0f1915ec2afe6c79e134fcccc29961
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 12:27:53 +0300

    DepartmanVM oluşturuldu

---
+++ StokTakipSistemi/ViewModels/DepartmanVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.ViewModels
+{
+    public class DepartmanVM
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Adi { get; set; }
+    }
+}
