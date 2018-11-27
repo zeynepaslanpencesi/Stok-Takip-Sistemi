c85ed233faba9e87a4719eab92b4a7404d6dfbaf
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 11:22:05 +0300

    IlceController sınıfının Index action'ı için View oluşturuldu

--- StokTakipSistemi/Controllers/IlceController.cs
+++ StokTakipSistemi/Controllers/IlceController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class IlceController : Controller
     {
+        private readonly IIlceService _ilceService;
+
+        public IlceController(IIlceService ilceService)
+        {
+            _ilceService = ilceService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _ilceService.GetAll();
+            return View(items);
+            
         }
     }
 }
