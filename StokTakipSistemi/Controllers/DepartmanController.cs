dbdecd8646a4e80e44447f543abc2e0abf1c1129
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-27 12:40:00 +0300

    DepartmanController sınıfının Create action'ı için HttpGet ve HttpPost metotları oluşturuldu

--- StokTakipSistemi/Controllers/DepartmanController.cs
+++ StokTakipSistemi/Controllers/DepartmanController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Models;
 using StokTakipSistemi.Services.Interfaces;
+using StokTakipSistemi.ViewModels;
 
 namespace StokTakipSistemi.Controllers
 {
@@ -19,5 +22,32 @@ namespace StokTakipSistemi.Controllers
             var items = _departmanService.GetAll();
             return View(items);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] DepartmanVM departman)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSameDepartmentExists = await _departmanService.IsExist(d => d.Adi == departman.Adi);
+
+                if (!isSameDepartmentExists)
+                {
+                    var mappedDepartman = Mapper.Map<Departman>(departman);
+                    await _departmanService.Create(mappedDepartman);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ViewBag.Errors = ModelState.Values.SelectMany(d => d.Errors);
+            return View(departman);
+        }
+
+
     }
 }
