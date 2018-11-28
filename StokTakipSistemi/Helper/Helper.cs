9e27ed940d93256f7a6e29ac4c8b4952dd93b029
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-28 14:10:46 +0300

    Helper klasörü oluşturuldu UrunController için Helper sınıfı oluşturuldu

---
+++ StokTakipSistemi/Helper/Helper.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using StokTakipSistemi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Helper
+{
+    public class Helper
+    {
+        private readonly StokTakipSistemiDbContext _dbContext;
+
+        public Helper(StokTakipSistemiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IList<SelectListItem> GetUrunSelectList(int? id = null)
+        {
+            IList<SelectListItem> selectList;
+
+            if (id == null)
+            {
+                selectList = _dbContext.Urun.ToList().
+                    Select(p => new SelectListItem() { Text = p.Adi, Value = p.Id.ToString() }).
+                    ToList();
+
+                return selectList;
+            }
+
+            selectList = new List<SelectListItem>();
+
+            foreach (var product in _dbContext.Urun.ToList())
+            {
+                if (product.Id == id)
+                {
+                    selectList.Add(new SelectListItem
+                    {
+                        Value = product.Id.ToString(),
+                        Text = product.Adi,
+                        Selected = true
+                    });
+                }
+                else
+                {
+                    selectList.Add(new SelectListItem
+                    {
+                        Value = product.Id.ToString(),
+                        Text = product.Adi
+                    });
+                }
+            }
+
+            return selectList;
+        }
+    }
+}
