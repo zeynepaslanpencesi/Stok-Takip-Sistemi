95e678dd001ecf28fddc92484cbb43ddb29b1141
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:12:18 +0300

    MarkaController sınıfının Create action'ı oluşturuldu

--- StokTakipSistemi/Controllers/MarkaController.cs
+++ StokTakipSistemi/Controllers/MarkaController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Models;
 using StokTakipSistemi.Services.Interfaces;
+using StokTakipSistemi.ViewModels;
 
 namespace StokTakipSistemi.Controllers
 {
@@ -20,6 +23,30 @@ namespace StokTakipSistemi.Controllers
             var items = _markaService.GetAll();
             return View(items);
             
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] MarkaVM marka)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSameBrandExists = await _markaService.IsExist(b => b.Adi == marka.Adi);
+
+                if (!isSameBrandExists)
+                {
+                    var mappedMarka = Mapper.Map<Marka>(marka);
+                    await _markaService.Create(mappedMarka);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
         }
     }
 }
