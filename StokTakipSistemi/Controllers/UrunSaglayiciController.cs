329ace1046d8d370172f5604462c659601ab00f4
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:47:55 +0300

    UrunSaglayiciController sınıfının Create action'ı için HttpGet ve HttpPost metotları oluşturuldu

--- StokTakipSistemi/Controllers/UrunSaglayiciController.cs
+++ StokTakipSistemi/Controllers/UrunSaglayiciController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Models;
 using StokTakipSistemi.Services.Interfaces;
+using StokTakipSistemi.ViewModels;
 
 namespace StokTakipSistemi.Controllers
 {
@@ -19,7 +22,31 @@ namespace StokTakipSistemi.Controllers
         {
             var items = _urunSaglayiciService.GetAll();
             return View(items);
-            ;
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] UrunSaglayiciVM urunSaglayici)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSameUrunSaglayiciExists = await _urunSaglayiciService.IsExist(p => p.Adi == urunSaglayici.Adi);
+
+                if (!isSameUrunSaglayiciExists)
+                {
+                    var mappedUrunSaglayici = Mapper.Map<UrunSaglayici>(urunSaglayici);
+                    await _urunSaglayiciService.Create(mappedUrunSaglayici);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
         }
     }
 }
