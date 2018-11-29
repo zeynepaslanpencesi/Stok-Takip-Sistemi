d5e1633e664079c8f6351a97adc15677e0c63438
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 13:08:41 +0300

    UrunTurController sınıfının Index actin'ı oluşturuldu

--- StokTakipSistemi/Controllers/UrunTurController.cs
+++ StokTakipSistemi/Controllers/UrunTurController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class UrunTurController : Controller
     {
+        private readonly IUrunTurService _urunTurService;
+
+        public UrunTurController(IUrunTurService urunTurService)
+        {
+            _urunTurService = urunTurService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var items = _urunTurService.GetAll();
+            return View(items);
+            
         }
     }
 }
