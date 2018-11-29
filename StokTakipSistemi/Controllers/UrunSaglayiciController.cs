a8b93d7e8c990ef9b24604c4439ef2657aba7382
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:40:33 +0300

    UrunSaglayiciController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/UrunSaglayiciController.cs
+++ StokTakipSistemi/Controllers/UrunSaglayiciController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class UrunSaglayiciController : Controller
     {
+        private readonly IUrunSaglayiciService _urunSaglayiciService;
+
+        public UrunSaglayiciController(IUrunSaglayiciService urunSaglayiciService)
+        {
+            _urunSaglayiciService = urunSaglayiciService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _urunSaglayiciService.GetAll();
+            return View(items);
+            ;
         }
     }
 }
