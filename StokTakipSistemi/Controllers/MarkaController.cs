c2ec21ff7296fe02a7ce7f174727fd63cef294e9
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:06:47 +0300

    MarkaController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/MarkaController.cs
+++ StokTakipSistemi/Controllers/MarkaController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class MarkaController : Controller
     {
+        private readonly IMarkaService _markaService;
+
+        public MarkaController(IMarkaService markaService)
+        {
+            _markaService = markaService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _markaService.GetAll();
+            return View(items);
+            
         }
     }
 }
