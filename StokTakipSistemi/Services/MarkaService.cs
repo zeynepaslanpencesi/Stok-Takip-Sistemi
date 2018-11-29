7cbacb724f2b7f8110710e3706b39dc0d3dfa52e
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:01:47 +0300

    MarkaService sınıfı oluşturuldu

---
+++ StokTakipSistemi/Services/MarkaService.cs
@@ -0,0 +1,17 @@
+﻿using StokTakipSistemi.Data;
+using StokTakipSistemi.Models;
+using StokTakipSistemi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services
+{
+    public class MarkaService: GenericRepository<Marka>, IMarkaService
+    {
+        public MarkaService(StokTakipSistemiDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
