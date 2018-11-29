53f2df35c97f6957305f4d6ea772a067ae9598e8
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:35:45 +0300

    UrunSaglayiciService sınıfı oluşturuldu

---
+++ StokTakipSistemi/Services/UrunSaglayiciService.cs
@@ -0,0 +1,17 @@
+﻿using StokTakipSistemi.Data;
+using StokTakipSistemi.Models;
+using StokTakipSistemi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services
+{
+    public class UrunSaglayiciService: GenericRepository<UrunSaglayici>, IUrunSaglayiciService
+    {
+        public UrunSaglayiciService(StokTakipSistemiDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
