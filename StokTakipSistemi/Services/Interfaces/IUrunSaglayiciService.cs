eec434da8f2377689c0aff1b6bb3aade0bc1ca21
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-11-29 12:33:51 +0300

    IUrunSaglayiciService interface'i oluşturuldu

---
+++ StokTakipSistemi/Services/Interfaces/IUrunSaglayiciService.cs
@@ -0,0 +1,12 @@
+﻿using StokTakipSistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services.Interfaces
+{
+    public interface IUrunSaglayiciService: IGenericRepository<UrunSaglayici>
+    {
+    }
+}
