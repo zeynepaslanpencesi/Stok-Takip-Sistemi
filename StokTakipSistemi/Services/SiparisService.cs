fb84dcf6d2f58ccc044cf3dfebaf09ba6edb802a
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-02 22:09:02 +0300

    SiparisService sınıfı oluşturuldu

---
+++ StokTakipSistemi/Services/SiparisService.cs
@@ -0,0 +1,16 @@
+﻿using StokTakipSistemi.Data;
+using StokTakipSistemi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services
+{
+    public class SiparisService: GenericRepository<Siparis>, ISiparisService
+    {
+        public SiparisService(StokTakipSistemiDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
