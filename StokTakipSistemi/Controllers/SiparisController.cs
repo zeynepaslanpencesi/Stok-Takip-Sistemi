31be137e2ebdcfaba2b64db03a1f27ef44fae2e1
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-02 22:26:24 +0300

    SiparisDTO sınıfı oluşturuldu

--- StokTakipSistemi/Controllers/SiparisController.cs
+++ StokTakipSistemi/Controllers/SiparisController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.DTOModels;
 using StokTakipSistemi.Services;
 using StokTakipSistemi.Services.Interfaces;
 
---
+++ StokTakipSistemi/DTOModels/SiparisDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.DTOModels
+{
+    public class SiparisDTO
+    {
+        public int Id { get; set; }
+        public int Adet { get; set; }
+        public int FaturaEditDTOId { get; set; }
+        public int UrunId { get; set; }
+    }
+}
