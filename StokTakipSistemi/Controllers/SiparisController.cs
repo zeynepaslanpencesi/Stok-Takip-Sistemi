fb18103f1ee0e34b2fe76ba5db2b29ca55e6999e
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-02 22:13:28 +0300

    interfaceler SiparisController'a tanıtıldı

--- StokTakipSistemi/Controllers/SiparisController.cs
+++ StokTakipSistemi/Controllers/SiparisController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class SiparisController : Controller
     {
+        private readonly ISiparisService _siparisService;
+        private readonly IUrunService _urunService;
+        private readonly IFaturaService _faturaService;
         public IActionResult Index()
         {
             return View();
