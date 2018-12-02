4499fc2af384f288b1ac2a51c75ac98dca192b02
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-02 22:11:15 +0300

    SiparisVM oluşturuldu

---
+++ StokTakipSistemi/ViewModels/SiparisVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.ViewModels
+{
+    public class SiparisVM
+    {
+        public int UrunId { get; set; }
+        public int Adet { get; set; }
+    }
+}
