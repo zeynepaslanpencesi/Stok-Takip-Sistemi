c15a45cecda67d088a2b09dda02fbc1fe41c4154
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-02 22:25:24 +0300

    SiparisSelfDTO sınıfı oluşturuldu

---
+++ StokTakipSistemi/DTOModels/SiparisSelfDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.DTOModels
+{
+    public class SiparisSelfDTO
+    {
+        public int Id { get; set; }
+        public int Adet { get; set; }
+        public int FaturaId { get; set; }
+        public string Urun { get; set; }
+        public int UrunId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
