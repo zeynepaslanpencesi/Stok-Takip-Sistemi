17fc45a00fa92f7147a644261979ede25a82088c
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-11 22:07:48 +0300

    FaturaDTO sınıfı oluşturuldu

---
+++ StokTakipSistemi/DTOModels/FaturaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.DTOModels
+{
+    public class FaturaDTO
+    {
+        public int Id { get; set; }
+        public DateTime Tarih { get; set; }
+        public string UrunSaglayici { get; set; }
+    }
+}
