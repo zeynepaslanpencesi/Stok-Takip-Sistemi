97a4359b72ead36d8f0aff914172ff4f3152abd2
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-11 22:36:29 +0300

    IFaturaService interface'ine DTO modeller eklendi

--- StokTakipSistemi/Controllers/FaturaController.cs
+++ StokTakipSistemi/Controllers/FaturaController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Helpers;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class FaturaController : Controller
     {
-        public IActionResult Index()
+        private readonly IFaturaService _faturaService;
+        private readonly ISiparisService _siparisService;
+        private readonly Helper _helper;
+
+        public FaturaController(IFaturaService faturaService, ISiparisService siparisService, Helper helper)
         {
-            return View();
+            _faturaService = faturaService;
+            _siparisService = siparisService;
+            _helper = helper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var items = await _faturaService.GetAllWithRelatives();
+            return View(items);
         }
     }
 }
--- StokTakipSistemi/Services/Interfaces/IFaturaService.cs
+++ StokTakipSistemi/Services/Interfaces/IFaturaService.cs
@@ -1,4 +1,5 @@
-﻿using StokTakipSistemi.Models;
+﻿using StokTakipSistemi.DTOModels;
+using StokTakipSistemi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,8 @@ namespace StokTakipSistemi.Services.Interfaces
 {
     public interface IFaturaService: IGenericRepository<Fatura>
     {
+        Task<FaturaDTO> GetWithRelative(int? id);
+        Task<List<FaturaDTO>> GetAllWithRelatives();
+        Task<FaturaDüzenleDTO> GetWithSiparisler(int? id);
     }
 }
