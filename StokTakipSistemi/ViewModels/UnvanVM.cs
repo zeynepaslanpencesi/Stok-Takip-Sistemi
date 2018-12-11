1f4762290dc28a4bc4a78c8eda5f57c3896dff35
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-11 23:47:58 +0300

    UnvanVM oluşturuldu

---
+++ StokTakipSistemi/ViewModels/UnvanVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.ViewModels
+{
+    public class UnvanVM
+    {
+        public string Adi { get; set; }
+        public byte Indirim { get; set; }
+    }
+}
