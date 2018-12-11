413d2ff3d40e5f0ab6d37cba441844a9d9b60405
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-11 23:50:36 +0300

    UnvanController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/UnvanController.cs
+++ StokTakipSistemi/Controllers/UnvanController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class UnvanController : Controller
     {
+        private readonly IUnvanService _unvanService;
+
+        public UnvanController(IUnvanService unvanService)
+        {
+            _unvanService = unvanService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _unvanService.GetAll();
+            return View(items);
         }
     }
 }
