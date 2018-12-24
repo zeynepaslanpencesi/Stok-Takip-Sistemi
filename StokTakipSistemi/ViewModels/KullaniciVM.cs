429f6cfdd43ae6ac7988babcf6db316c77d8af01
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-24 10:59:03 +0300

    KullaniciVM sınıfı oluşturuldu

---
+++ StokTakipSistemi/ViewModels/KullaniciVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.ViewModels
+{
+    public class KullaniciVM
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Email { get; set; }
+        [StringLength(15, MinimumLength = 8)]
+        public string Sifre { get; set; }
+        public DateTime Dogum_gunu { get; set; }
+        public DateTime Baslangic_Tarihi { get; set; }
+    }
+}
