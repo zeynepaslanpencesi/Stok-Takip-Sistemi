19ff4dc7f01dd4b63f5d779797b71f9a41798c1b
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-24 11:13:34 +0300

    KullaniciController sınıfının Index action'ı oluşturuldu

--- StokTakipSistemi/Controllers/KullaniciController.cs
+++ StokTakipSistemi/Controllers/KullaniciController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StokTakipSistemi.Helpers;
+using StokTakipSistemi.Services.Interfaces;
 
 namespace StokTakipSistemi.Controllers
 {
     public class KullaniciController : Controller
     {
+        private readonly IKullaniciService _kullaniciService;
+        private readonly Helper _helper;
+
+        public KullaniciController(IKullaniciService kullaniciService, Helper helper)
+        {
+            _kullaniciService = kullaniciService;
+            _helper = helper;
+        }
         public IActionResult Index()
         {
-            return View();
+            var items = _kullaniciService.GetAll();
+            return View(items);
         }
+
+        
     }
 }
