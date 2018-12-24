466ee13a699acbde765cc6e69abb20f1a4a967ea
Zeynep Aslanpençesi <zeyneppasp@gmail.com>
2018-12-24 10:55:33 +0300

    KullaniciService sınıfı oluşturuldu

---
+++ StokTakipSistemi/Services/KullaniciService.cs
@@ -0,0 +1,19 @@
+﻿using StokTakipSistemi.Data;
+using StokTakipSistemi.Models;
+using StokTakipSistemi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StokTakipSistemi.Services
+{
+    public class KullaniciService: GenericRepository<Kullanici>, IKullaniciService
+    {
+
+        public KullaniciService(StokTakipSistemiDbContext dbContext): base(dbContext)
+        {
+
+        }
+    }
+}
